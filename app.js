e74c1dee6ae8c598a1e01b4a26a53aa9fe4b5d3d
SubhamSaha9 <sahasubham7478@gmail.com>
2023-10-31 01:53:23 +0530

    website ready

--- app.js
+++ app.js
@@ -23,27 +23,29 @@ init();
 
 let cursor = document.querySelector("#cursor");
 let main = document.querySelector(".main");
-let video = document.querySelector("video");
-main.addEventListener("mousemove", function (desc) {
-    cursor.style.left = desc.x + "px";
-    cursor.style.top = desc.y + "px";
+let video = document.querySelectorAll("video");
+document.addEventListener("mousemove", function (desc) {
+    cursor.style.left = desc.x + 5 + "px";
+    cursor.style.top = desc.y + 5 + "px";
 })
-video.addEventListener("mouseenter", function () {
-    cursor.innerHTML = `<p>Sound on</p>`
-    cursor.classList.add("sound");
-    cursor.classList.remove("cursor");
-})
-video.addEventListener("mouseleave", function () {
-    cursor.innerText = "";
-    cursor.classList.add("cursor");
-    cursor.classList.remove("sound");
+video.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+        cursor.innerHTML = `<p>Sound on</p>`
+        cursor.classList.add("sound");
+        cursor.classList.remove("cursor");
+    })
+    elem.addEventListener("mouseleave", function () {
+        cursor.innerText = "";
+        cursor.classList.add("cursor");
+        cursor.classList.remove("sound");
+    })
 })
 
 let tl = gsap.timeline({
     scrollTrigger: {
         trigger: ".page1 h1",
         scroller: ".main",
-        markers: true,
+        // markers: true,
         start: "top 27%",
         end: "top 0",
         scrub: 3
@@ -65,7 +67,7 @@ let tl2 = gsap.timeline({
     scrollTrigger: {
         trigger: ".page1 h1",
         scroller: ".main",
-        markers: true,
+        // markers: true,
         start: "top -116%",
         end: "top -130%",
         scrub: 3
@@ -79,7 +81,7 @@ let tl3 = gsap.timeline({
     scrollTrigger: {
         trigger: ".page1 h1",
         scroller: ".main",
-        markers: true,
+        // markers: true,
         start: "top -280%",
         end: "top -300%",
         scrub: 3
@@ -87,4 +89,40 @@ let tl3 = gsap.timeline({
 })
 tl3.to(".main", {
     backgroundColor: "#0F0D0D"
+})
+
+let boxes = document.querySelectorAll(".box");
+boxes.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+        let src = elem.getAttribute("data-image");
+        cursor.style.width = "470px";
+        cursor.style.height = "280px";
+        cursor.style.borderRadius = "0";
+        cursor.style.zIndex = "30";
+        cursor.style.backgroundImage = `url(${src})`;
+    })
+    elem.addEventListener("mouseleave", function () {
+        cursor.style.width = "20px";
+        cursor.style.height = "20px";
+        cursor.style.borderRadius = "50%";
+        cursor.style.backgroundImage = `none`;
+    })
+})
+let h4 = document.querySelectorAll("#nav h4");
+let purple = document.querySelector("#purple");
+let mid = document.querySelector(".effect");
+h4.forEach(function (elem) {
+    elem.addEventListener("mouseenter", () => {
+        console.log(elem.innerText);
+        purple.style.display = "block";
+        purple.style.opacity = "1";
+        mid.innerHTML = `<marquee scrollamount="25"><h1 style="color:#111;font-family: PP mori;">${elem.innerText}  ${elem.innerText}  ${elem.innerText}  ${elem.innerText}  ${elem.innerText}  ${elem.innerText}  ${elem.innerText} </h1></marquee>`;
+
+
+    })
+    elem.addEventListener("mouseleave", () => {
+        purple.style.display = "none";
+        purple.style.opacity = "0";
+
+    })
 })
